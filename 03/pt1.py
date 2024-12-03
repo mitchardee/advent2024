8bbc8419286450291202991c56315e4a883065f9
Mitch Hardee <emh3fm@virginia.edu>
2024-12-03 12:53:10 -0500

    Use regex for day 3.

--- 03/pt1.py
+++ 03/pt1.py
@@ -1,15 +1,10 @@
-line = ''.join(open("input.txt").readlines())
+import re
+
+line = ''.join(open("testInput.txt").readlines())
 ret = 0
 
-for i in range(len(line)):
-    if line[i:i+4] == "mul(":
-        if ')' in line[i+7:i+12]:
-            close = line[i+7:i+12].index(')')
-            subS = line[i+4:i+7+close]
-
-            if ',' in subS[1:-1]:
-                comma = subS[1:-1].index(',') + 1
-                if subS[:comma].isdigit() and subS[comma+1:].isdigit():
-                    ret += int(subS[:comma]) * int(subS[comma+1:])
+phrases = re.findall('mul\(\d\d?\d?,\d\d?\d?\)', line)
+for x in phrases:
+    ret += int(x[4:x.index(',')]) * int(x[x.index(',')+1:-1])
 
 print(ret)
--- 03/pt2.py
+++ 03/pt2.py
@@ -1,20 +1,18 @@
+import re
+
 line = ''.join(open("input.txt").readlines())
 ret = 0
 enabledBit = 1
 
-for i in range(len(line)):
-    if line[i:i+4] == "do()":
-        enabledBit = 1
-    if line[i:i+7] == "don't()":
-        enabledBit = 0
-    if line[i:i+4] == "mul(":
-        if ')' in line[i+7:i+12]:
-            close = line[i+7:i+12].index(')')
-            subS = line[i+4:i+7+close]
+phrases = re.findall(r'(mul\(\d\d?\d?,\d\d?\d?\)|do\(\)|don\'t\(\)){1}', line)
+for x in phrases:
+    match x:
+        case "do()":
+            enabledBit = 1
+        case "don't()":
+            enabledBit = 0
+        case default:
+            ret += int(x[4:x.index(',')]) * int(x[x.index(',')+1:-1]) * enabledBit
 
-            if ',' in subS[1:-1]:
-                comma = subS[1:-1].index(',') + 1
-                if subS[:comma].isdigit() and subS[comma+1:].isdigit():
-                    ret += int(subS[:comma]) * int(subS[comma+1:]) * enabledBit
 
 print(ret)
