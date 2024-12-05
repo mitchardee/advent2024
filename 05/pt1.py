fea2969136a4076a275b9913c7b87c4b4996f2e6
Mitch Hardee <emh3fm@virginia.edu>
2024-12-05 11:12:26 -0500

    Minor day 5 optimizations.

--- 05/pt1.py
+++ 05/pt1.py
@@ -1,7 +1,7 @@
 from collections import defaultdict
 
 lines = open("input.txt").readlines()
-befores, afters = defaultdict(set), defaultdict(set)
+afters = defaultdict(set)
 j = 0
 ret = 0
 
@@ -10,19 +10,18 @@ for i, line in enumerate(lines):
         j = i+1
         break
     a, b = int(line[:line.index('|')]), int(line[line.index('|')+1:])
-
-    befores[b].add(a)
     afters[a].add(b)
 
 
 for line in lines[j:]:
     n = [int(x) for x in line.strip().split(',')]
-    bad = False
-    for i, x in enumerate(n):
-        for y in n[i+1:]:
-            if y in befores[x]:
-                bad = True
-    if not bad:
+
+    if not any(
+        any(
+            x in afters[y] for y in n[i+1:]
+            ) 
+        for i,x in enumerate(n)
+        ):
         ret += int(n[len(n)//2])
 
 print(ret)
--- 05/pt2.py
+++ 05/pt2.py
@@ -2,7 +2,7 @@ from collections import defaultdict
 from functools import cmp_to_key
 
 lines = open("input.txt").readlines()
-befores, afters = defaultdict(set), defaultdict(set)
+afters = defaultdict(set)
 j = 0
 ret = 0
 
@@ -12,19 +12,23 @@ for i, line in enumerate(lines):
         break
     a, b = int(line[:line.index('|')]), int(line[line.index('|')+1:])
 
-    befores[b].add(a)
     afters[a].add(b)
 
 
 for line in lines[j:]:
     n = [int(x) for x in line.strip().split(',')]
-    bad = False
-    for i, x in enumerate(n):
-        for y in n[i+1:]:
-            if y in befores[x]:
-                bad = True
-    if bad:
-        n.sort(key=cmp_to_key(lambda a, b: 1 if a in afters[b] else -1 if b in afters[a] else 0))
+
+    if any(
+        any(
+            x in afters[y] for y in n[i+1:]
+            ) 
+        for i,x in enumerate(n)
+        ):
+        n.sort(key=cmp_to_key(
+            lambda a, b: 1 if a in afters[b] 
+            else -1 if b in afters[a] 
+            else 0)
+            )
         ret += n[len(n)//2]
 
 print(ret)
