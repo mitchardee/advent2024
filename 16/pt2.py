3acd2e2d176de0194225a75ed5e8d7913fbc19f1
Mitch Hardee <emh3fm@virginia.edu>
2024-12-16 01:08:19 -0500

    Minor heap memory optimization.

--- 16/pt2.py
+++ 16/pt2.py
@@ -3,34 +3,37 @@ from heapq import *
 from math import inf
 
 
-# lines = [x.strip() for x in open("testInput.txt").readlines()]
-lines = [x.strip() for x in open("input.txt").readlines()]
+lines = [x.strip() for x in open("testInput.txt").readlines()]
+# lines = [x.strip() for x in open("input.txt").readlines()]
 
 dirs = ((0,1), (-1,0), (0,-1), (1,0))
 
 #Score, dir, r, c
-h = [(0, 0, len(lines) - 2, 1, ((len(lines) - 2, 1),))]
+h = [(0, 0, len(lines) - 2, 1, (0, len(lines) - 2, 1))]
 goal = (1, len(lines[0]) - 2)
 bestScorePath = defaultdict(set)
 visited = {}
 bestScore = inf
 
 while h[0][0] <= bestScore:
-    (s, d, r, c, path) = heappop(h)
+    (s, d, r, c, prev) = heappop(h)
     if (r, c) == goal:
         bestScore = s
 
-    if (d, r, c) not in visited:
-        visited[(d, r, c)] = s
+    drc = (d, r, c)
+
+    if drc not in visited:
+        visited[drc] = s
+        bestScorePath[drc].add((r,c))
         
 
-    if visited[(d, r, c)] == s:
-        bestScorePath[(d, r, c)] |= set(path)
+    if visited[drc] == s:
+        bestScorePath[drc] |= bestScorePath[prev]
         nr, nc = r + dirs[d][0], c + dirs[d][1]
         if lines[nr][nc] != '#':
-            heappush(h, (s+1, d, nr, nc, path + ((nr,nc),)))
-        heappush(h, (s + 1000, (d+1)%4, r, c, path))
-        heappush(h, (s + 1000, (d-1)%4, r, c, path))
+            heappush(h, (s+1, d, nr, nc, drc))
+        heappush(h, (s + 1000, (d+1)%4, r, c, drc))
+        heappush(h, (s + 1000, (d-1)%4, r, c, drc))
         
 
 pathSpots = set()
